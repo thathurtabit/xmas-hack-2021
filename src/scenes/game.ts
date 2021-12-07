ad38c1c77c83a969fcfdbd5e9069fc6e61d64f57
Jenni Foster <Jenni.Foster@autotrader.co.uk>
2021-12-07 12:32:42 +0000

    add map

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,6 +1,7 @@
 import "phaser";
 import { EScenes } from "../settings/enums";
 import { fontFamily } from "../settings/constants";
+import TilemapLayer = Phaser.Tilemaps.TilemapLayer;
 
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -8,40 +9,61 @@ export default class Game extends Phaser.Scene {
   }
 
   preload() {
-    // Preload assets here i.e...
-    // this.load.image("logo", "assets/phaser3-logo.png");
+    this.load.image('white', 'assets/map/white-square.png');
+    this.load.image('black', 'assets/map/black-square.png');
+    this.load.tilemapTiledJSON('map', 'assets/map/map-json.json');
   }
 
   create() {
-    this.cameras.main.setBackgroundColor("#313c53");
-    const screenCenterX =
-      this.cameras.main.worldView.x + this.cameras.main.width / 2;
-    const screenCenterY =
-      this.cameras.main.worldView.y + this.cameras.main.height / 2;
+    const map = this.make.tilemap({ key: 'map' });
+    const whiteTileset = map.addTilesetImage('map-background', 'white', 8, 8);
+    const blackTileset = map.addTilesetImage('faces', 'black', 8, 8);
 
-    this.add
-      .text(screenCenterX, screenCenterY + 30, `GAME STUFF`, {
-        font: `30px ${fontFamily}`,
-      })
-      .setOrigin(0.5)
-      .setAlign("center");
+    const backgroundLayer = map.createLayer('background', whiteTileset);
+    
+    const edgesLayer = map.createLayer('walls', whiteTileset);
+    const facesLayer = map.createLayer('faces', blackTileset);
+    
 
-    this.add
-      .text(screenCenterX, 500, `Game Over`, {
-        font: `40px ${fontFamily}`,
-        padding: { x: 20, y: 10 },
-        backgroundColor: "#fff",
-        color: "#000",
-      })
-      .setOrigin(0.5)
-      .setAlign("center");
+    const collidingLayers: Array<TilemapLayer> = [edgesLayer, facesLayer]
+    this.setCollision(collidingLayers)
 
-    this.input.on(
-      "pointerup",
-      () => {
-        this.scene.start(EScenes.GAME_OVER);
-      },
-      this
-    );
+
+
+    // const screenCenterX =
+    //   this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    // const screenCenterY =
+    //   this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+    // this.add
+    //   .text(screenCenterX, screenCenterY + 30, `GAME STUFF`, {
+    //     font: `30px ${fontFamily}`,
+    //   })
+    //   .setOrigin(0.5)
+    //   .setAlign("center");
+
+    // this.add
+    //   .text(screenCenterX, 500, `Game Over`, {
+    //     font: `40px ${fontFamily}`,
+    //     padding: { x: 20, y: 10 },
+    //     backgroundColor: "#fff",
+    //     color: "#000",
+    //   })
+    //   .setOrigin(0.5)
+    //   .setAlign("center");
+
+    // this.input.on(
+    //   "pointerup",
+    //   () => {
+    //     this.scene.start(EScenes.GAME_OVER);
+    //   },
+    //   this
+    // );
+  }
+
+  private setCollision(collidingLayers: Array<TilemapLayer>) {
+    collidingLayers.forEach((layer) => {
+      layer.setCollisionByProperty({ collides: true });
+    });
   }
 }
