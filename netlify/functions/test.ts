1bc8e2ce057d0bb9e73fc73653add57e538e0555
gnoeley <leonpelech@gmail.com>
2021-12-08 11:26:49 +0000

    Add test Netlify function
    - Now able to run locally with watch using: npx netlify-cli dev
    - Serves functions under URL e.g. <server>/.netlify/functions/test
    - Install Netlify Functions TypeScript definitions
    - Allow configuring environment using a .env file
    - Remove checked-in static JS bundle in dist/ and update .gitignore
    to prevent it and compiled functions being added again

---
+++ netlify/functions/test.ts
@@ -0,0 +1,10 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Hello, ${process.env.TEST_COOL_ENV_VARIABLE}` }),
+    };
+};
+
+export { handler };
